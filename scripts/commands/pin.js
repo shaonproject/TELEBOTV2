18230ec183588510d2c7ea2ea1274c340bda805b
shaonproject <153928138+shaonproject@users.noreply.github.com>
2025-04-17 15:31:43 +0600

    Update pin.js

--- scripts/commands/pin.js
+++ scripts/commands/pin.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = {
     config: {
-        name: "pinterest",
+        name: "pic",
         aliases: ["pin", "pinterestsearch"],
         role: 0, // All users can use this command
         cooldowns: 5,
@@ -19,11 +19,11 @@ module.exports = {
         }
 
         // Join the arguments and split by "|"
-        const input = args.join(" ").split("|");
+        const input = args.join(" ").split("-");
         const query = input[0].trim();
         const number = parseInt(input[1]) || 6; // Default to 6 if number is not provided
 
-        const apiUrl = `https://www.samirxpikachu.run.place/pinterest?query=${encodeURIComponent(query)}&number=${number}`;
+        const apiUrl = `https://noobs-api-sable.vercel.app/pinterest?search=${encodeURIComponent(query)}`;
 
         // Send a pre-processing message
         const preMessage = await bot.sendMessage(chatId, "🔍 | Searching for images...", { replyToMessage: msg.message_id });
@@ -32,9 +32,9 @@ module.exports = {
             // Make a request to the Pinterest API
             const response = await axios.get(apiUrl);
 
-            if (response.data.status === 200 && response.data.result.length > 0) {
+            if (response.data.status === 200 && response.data.data.length > 0) {
                 // Prepare an array of image objects for the album
-                const images = response.data.result.map(imageUrl => ({ type: 'photo', media: imageUrl }));
+                const images = response.data.data.map(imageUrl => ({ type: 'photo', media: imageUrl }));
 
                 // Send the album
                 await bot.sendMediaGroup(chatId, images, { caption: `Here are images for "${query}"`, replyToMessage: msg.message_id });
